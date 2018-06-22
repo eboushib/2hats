bd90e53507b0ab795e4329e98aac54eb89e9c440
Kelvin <kelvin.li@vontechnology.com>
2018-06-22 17:47:18 +1000

    added js to carry email to app.2hats.com.au

---
+++ js/main.js
@@ -0,0 +1,10 @@
+var captureEmail = function(id) {
+	$('.bp'+id+'-getaheadbutton a').on('click',function(){
+		window.open('https://app.2hats.com.au/signup?email='+$('.bp'+id+'-textyouremail').val());
+	});
+}
+$( document ).ready(function(){
+	captureEmail(1);
+	captureEmail(2);
+	captureEmail(3);
+});
