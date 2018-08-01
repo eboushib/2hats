b8841fe7bbeb40dd5789eddac11b2ed8c987952a
Kelvin <kelvin.li@vontechnology.com>
2018-08-01 11:45:29 +0100

    Beautified the code

--- js/main.js
+++ js/main.js
@@ -1,9 +1,9 @@
-var captureEmail = function(id) {
-	$('.bp'+id+'-getaheadbutton a').on('click',function(){
-		window.open('https://app.2hats.com.au/signup?email='+$('.bp'+id+'-textyouremail').val());
+var captureEmail = function (id) {
+	$('.bp' + id + '-getaheadbutton a').on('click', function () {
+		window.open('https://app.2hats.com.au/signup?email=' + $('.bp' + id + '-textyouremail').val());
 	});
-}
-$( document ).ready(function(){
+};
+$(document).ready(function () {
 	captureEmail(1);
 	captureEmail(2);
 	captureEmail(3);
