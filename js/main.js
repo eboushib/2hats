9b728b903679ac8dd4e2270af092d1ee61934919
Kelvin <kelvin.li@vontechnology.com>
2018-08-04 21:01:44 +0100

    added home page

--- js/main.js
+++ js/main.js
@@ -7,4 +7,31 @@ $(document).ready(function () {
 	captureEmail(1);
 	captureEmail(2);
 	captureEmail(3);
+
+	// Banner height
+	if ($(window).width() > 992) {
+		$('header .banner').css('height', $(window).height() - $('nav').outerHeight());
+	}
+
+	// Line 1 - start --> dest-1
+	var myLine1 = new LeaderLine(
+		document.getElementById('start'),
+		document.getElementById('dest-1'), {
+			color: '#fff',
+			size: 6,
+			startPlug: 'disc',
+			endPlug: 'disc',
+			path: 'arc'
+		}
+	);
+	var myLine2 = new LeaderLine(
+		document.getElementById('dest-1'),
+		document.getElementById('dest-2'), {
+			color: '#fff',
+			size: 6,
+			startPlug: 'disc',
+			endPlug: 'disc',
+			path: 'arc'
+		}
+	);
 });
