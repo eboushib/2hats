e9876454a46628a36cb4acd9ab983dc0551f2dc4
Kelvin <kelvin.li@vontechnology.com>
2018-08-14 13:59:18 +0100

    Adjusted bullets numbering

--- js/main.js
+++ js/main.js
@@ -40,13 +40,13 @@ $(document).ready(function () {
 	// desktop view
 	var ourProcessSectionOffset = $('.our-process').offset().top;
 
-	var step2offset = ($('.bullet-1').offset().top - ($('.step-2').height() / 2)) - ourProcessSectionOffset + 18;
+	var step2offset = ($('.bullet-2').offset().top - ($('.step-2').height() / 2)) - ourProcessSectionOffset + 18;
 	$('.step-2').css('top', step2offset);
 
-	var step3offset = ($('.bullet-2').offset().top - ($('.step-3').height() / 2)) - ourProcessSectionOffset + 18;
+	var step3offset = ($('.bullet-3').offset().top - ($('.step-3').height() / 2)) - ourProcessSectionOffset + 18;
 	$('.step-3').css('top', step3offset);
 
-	var step4offset = ($('.bullet-3').offset().top - ($('.step-4').height() / 2)) - ourProcessSectionOffset + 18;
+	var step4offset = ($('.bullet-4').offset().top - ($('.step-4').height() / 2)) - ourProcessSectionOffset + 18;
 	$('.step-4').css('top', step4offset);
 
 
@@ -61,17 +61,17 @@ $(document).ready(function () {
 		var step2Offset = $('.step-2').offset().top,
 			step2PaddingTop = parseInt($('.step-2').css('padding-top').slice(0, -2)),
 			bullet1Offset = step2Offset - sectionOffset + step2PaddingTop;
-		$('.bullet-1').css('top', bullet1Offset);
+		$('.bullet-2').css('top', bullet1Offset);
 		// step-3
 		var step3Offset = $('.step-3').offset().top,
 			step3PaddingTop = parseInt($('.step-3').css('padding-top').slice(0, -2)),
 			bullet2Offset = step3Offset - sectionOffset + step3PaddingTop;
-		$('.bullet-2').css('top', bullet2Offset);
+		$('.bullet-3').css('top', bullet2Offset);
 		// step-4
 		var step4Offset = $('.step-4').offset().top,
 			step4PaddingTop = parseInt($('.step-4').css('padding-top').slice(0, -2)),
 			bullet3Offset = step4Offset - sectionOffset + step4PaddingTop;
-		$('.bullet-3').css('top', bullet3Offset);
+		$('.bullet-4').css('top', bullet3Offset);
 		// step-5
 		var step5Offset = $('.step-5').offset().top,
 			artwork2Offset = step5Offset - sectionOffset - 40;
