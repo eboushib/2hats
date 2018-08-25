985bfe1e6ad2120b28288fcaf7f3f4a8372036f3
Kelvin <kelvin.li@vontechnology.com>
2018-08-25 09:42:59 +0100

    start solving issue Minjie

--- js/main.js
+++ js/main.js
@@ -9,20 +9,20 @@ $(document).ready(function () {
 	captureEmail(3);
 
 	// trigger css animations on scroll
-	$(window).scroll(function(){
-		$('.our-process .step-1 h3, .our-process .step-1 p, .our-process .step-2 h3, .our-process .step-2 p, .our-process .step-4 h3, .our-process .step-4 p').each(function(){
-			if ( $(window).scrollTop() + ( $(window).height() * 7/8 ) > $(this).offset().top ) {
+	$(window).scroll(function () {
+		$('.our-process .step-1 h3, .our-process .step-1 p, .our-process .step-2 h3, .our-process .step-2 p, .our-process .step-4 h3, .our-process .step-4 p').each(function () {
+			if ($(window).scrollTop() + ($(window).height() * 7 / 8) > $(this).offset().top) {
 				$(this).addClass('animatedOnScrollToLeft');
 			}
 
 		});
-		$('.our-process .step-3 h3, .our-process .step-3 p, .our-process .step-5 h3, .our-process .step-5 p').each(function(){
-			if ( $(window).scrollTop() + ( $(window).height() * 7/8 ) > $(this).offset().top ) {
+		$('.our-process .step-3 h3, .our-process .step-3 p, .our-process .step-5 h3, .our-process .step-5 p').each(function () {
+			if ($(window).scrollTop() + ($(window).height() * 7 / 8) > $(this).offset().top) {
 				$(this).addClass('animatedOnScrollToRight');
 			}
 		});
-		$('.how-it-works, .testimonials, .cta').each(function(){
-			if ( $(window).scrollTop() + ( $(window).height() * 7/8 ) > $(this).offset().top ) {
+		$('.how-it-works, .testimonials, .cta').each(function () {
+			if ($(window).scrollTop() + ($(window).height() * 7 / 8) > $(this).offset().top) {
 				$(this).addClass('animatedOnScrollToTop');
 			}
 		});
@@ -37,47 +37,47 @@ $(document).ready(function () {
 	}
 
 	// Adjust text and timeline bullets
-	// desktop view
 	var ourProcessSectionOffset = $('.our-process').offset().top;
 
-	var step2offset = ($('.bullet-2').offset().top - ($('.step-2').height() / 2)) - ourProcessSectionOffset + 18;
-	$('.step-2').css('top', step2offset);
-
-	var step3offset = ($('.bullet-3').offset().top - ($('.step-3').height() / 2)) - ourProcessSectionOffset + 18;
-	$('.step-3').css('top', step3offset);
-
-	var step4offset = ($('.bullet-4').offset().top - ($('.step-4').height() / 2)) - ourProcessSectionOffset + 18;
-	$('.step-4').css('top', step4offset);
-
-
-	// adjust bullets offset top in mobile view
-	/*
-	if ($(window).width() <= 992) {
-		// step-1
-		var step1Offset = $('.step-1').offset().top,
-			artwork1Offset = step1Offset - ourProcessSectionOffset;
-		$('.artwork-1').css('top', artwork1Offset);
-		// step-2
-		var step2Offset = $('.step-2').offset().top,
-			step2PaddingTop = parseInt($('.step-2').css('padding-top').slice(0, -2)),
-			bullet1Offset = step2Offset - sectionOffset + step2PaddingTop;
-		$('.bullet-2').css('top', bullet1Offset);
-		// step-3
-		var step3Offset = $('.step-3').offset().top,
-			step3PaddingTop = parseInt($('.step-3').css('padding-top').slice(0, -2)),
-			bullet2Offset = step3Offset - sectionOffset + step3PaddingTop;
-		$('.bullet-3').css('top', bullet2Offset);
-		// step-4
-		var step4Offset = $('.step-4').offset().top,
-			step4PaddingTop = parseInt($('.step-4').css('padding-top').slice(0, -2)),
-			bullet3Offset = step4Offset - sectionOffset + step4PaddingTop;
-		$('.bullet-4').css('top', bullet3Offset);
-		// step-5
-		var step5Offset = $('.step-5').offset().top,
-			artwork2Offset = step5Offset - sectionOffset - 40;
-		$('.artwork-2').css('top', artwork2Offset);
+	// desktop view
+	if ($(window).width() > 992) {
+		for (var i = 2; i < 5; i++) {
+			$('.step-' + i).css({
+				top: ($('.bullet-' + i).offset().top - ($('.step-' + i).height() / 2)) - ourProcessSectionOffset + 18
+			});
+		}
 	}
-	*/
 
+	// mobile view
+	if ($(window).width() <= 992) {
 
+		// step-1
+		$('.artwork-1').css({
+			// top: $('.step-1').offset().top - ourProcessSectionOffset - 50
+			top: $('.step-1').offset().top - ourProcessSectionOffset - 40
+		});
+
+		// step-5
+		$('.artwork-2').css({
+			// top: $('.step-5').offset().top - ourProcessSectionOffset - 60
+			top: $('.step-5').offset().top - ourProcessSectionOffset - 40
+		});
+
+		// step 2, 3 & 4
+		for (var j = 2; j < 5; j++) {
+			$('.bullet-' + j).css({
+				top: $('.step-' + j).offset().top - ourProcessSectionOffset + parseInt($('.step-' + j).css('padding-top').slice(0, -2)) + 10
+			});
+		}
+	}
+
+	// toggle navbar in mobile view
+	$('#navbar-toggler').click(function () {
+		$('.top-menu .navbar').slideToggle(500);
+		$(this).toggleClass('df');
+		$('.navbar-toggler span').toggleClass('pabs');
+		$('.navbar-toggler span:first-child').toggleClass('rotate-minus-40');
+		$('.navbar-toggler span:nth-child(2)').toggleClass('hidden');
+		$('.navbar-toggler span:nth-child(3)').toggleClass('rotate-plus-40');
+	});
 });
