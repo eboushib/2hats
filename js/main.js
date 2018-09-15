ed6fdf8d0a377f85b7ee7a3fec480d84eaaae55a
Kelvin <kelvin.li@vontechnology.com>
2018-09-15 06:05:47 +0100

    fixed step title and descp overlap on 790px screen width

--- js/main.js
+++ js/main.js
@@ -41,7 +41,7 @@ $(document).ready(function () {
 				$(this).addClass('animatedOnScrollToLeftForward');
 			}
 		});
-		if ($(window).width() <= 768){
+		if ($(window).outerWidth() <= 768){
 			$('.our-process.selection-process .step-description').each(function () {
 				if ($(window).scrollTop() + ($(window).height() * 7 / 8) > $(this).offset().top) {
 					$(this).addClass('animatedOnScrollToLeftForward');
